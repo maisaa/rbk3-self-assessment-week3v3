bc907c99366d759d723bddeb473042d5669b5ad2
maisaa <maisaa.alkhedr@gmail.com>
2017-09-09 09:41:35 +0300

    easyBackbone

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -8,5 +8,15 @@ var Fish = Backbone.Model.extend({
     description: 'Regular old fish',
     displayInfo: false
   }
+  toggleDescription(){
+  	//in side this function i would check if the events for this fish is clicked
+  	// then i will change the state of toggle on or off
+  	// so i need to add new variable with name toggle (bollen) 
+  	//if(this.Model.get(this.clicked))
+
+  }
 
 });
+//[ ] Inside of `fishModel.js`, create a `toggleDescription` function such that when a fish <tr> is clicked, the fish description will toggle on or off. DO NOT MODIFY any other files.
+
+// 
